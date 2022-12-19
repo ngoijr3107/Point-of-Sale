ac9bf92fc52d8e37af3da19e6e43a81a1c452122
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-19 18:04:00 +0300

    expiry date column

--- Modules/Product/Http/Requests/UpdateProductRequest.php
+++ Modules/Product/Http/Requests/UpdateProductRequest.php
@@ -20,6 +20,7 @@ class UpdateProductRequest extends FormRequest
             'product_code' => ['required', 'string', 'max:255', 'unique:products,product_code,' . $this->product->id],
             'product_barcode_symbology' => ['required', 'string', 'max:255'],
             'product_unit' => ['required', 'string', 'max:255'],
+            'expiry_date' => ['required', 'string', 'max:255'],
             'product_quantity' => ['required', 'integer', 'min:1'],
             'product_cost' => ['required', 'numeric', 'max:2147483647'],
             'product_price' => ['required', 'numeric', 'max:2147483647'],
