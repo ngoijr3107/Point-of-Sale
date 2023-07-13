120c6f6b944b56991ab5b89f76b4319f8b9d0f74
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-07-13 14:56:43 +0300

    add customer address and TIN

--- Modules/Sale/Resources/views/print.blade.php
+++ Modules/Sale/Resources/views/print.blade.php
@@ -37,6 +37,8 @@
                             <div>{{ $customer->address }}</div>
                             {{-- <div>Email: {{ $customer->customer_email }}</div> --}}
                             <div>Phone: {{ $customer->customer_phone }}</div>
+                            <div>TIN: {{ $customer->customer_tin }}</div>
+                            <div>Address: {{ $customer->address }}</div>
                         </div>
 
                         <div class="col-xs-4 mb-3 mb-md-0">
